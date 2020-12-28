be2543bbdd7e7e6f353e411177d1c6559d57e86f
Mei <meiluwork@gmail.com>
2020-12-27 19:26:36 -0500

    Raffle merge test

--- src/Components/App/Navigation.jsx
+++ src/Components/App/Navigation.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import {
     AppBar,
     Toolbar,
@@ -8,6 +8,7 @@ import {
 } from '@material-ui/core';
 import CSALogo from '../../Assets/Images/CSALogo.svg';
 import { Link } from 'react-router-dom';
+import FirebaseContext from '../../Firebase/FirebaseContext';
 
 
 const useStyles = makeStyles(theme => ({
@@ -28,17 +29,19 @@ function Navigation(props) {
     const classes = useStyles();
 
     return (
-        <div>
-            <AppBar>
-                <Toolbar>
-                    <img src={CSALogo} className={classes.logo} alt='' />
-                    <Typography align='left' variant='h6' color='secondary' className={classes.heading}>Lunar Banquet 2020</Typography>
-                    <Button color='inherit' onClick={logout()}>Log Out</Button>
-                    <Button color='inherit'><Link to="/login">Login</Link></Button>
-                </Toolbar>
-            </AppBar>
-            <Toolbar />
-        </div>
+        <FirebaseContext.Consumer>
+            <div>
+                <AppBar>
+                    <Toolbar>
+                        <img src={CSALogo} className={classes.logo} alt='' />
+                        <Typography align='left' variant='h6' color='secondary' className={classes.heading}>Lunar Banquet 2020</Typography>
+                        <Button color='inherit' onClick={logout()}>Log Out</Button>
+                        <Button color='inherit'><Link to="/login">Sign In</Link></Button>
+                    </Toolbar>
+                </AppBar>
+                <Toolbar />
+            </div>
+        </FirebaseContext.Consumer>
     );
 }
 
