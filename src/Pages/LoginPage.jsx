e7dc918cbb2b08a21b3de6751f4b34681d945dcb
Mei <meiluwork@gmail.com>
2020-12-29 19:56:01 -0500

    Message on login page

--- src/Pages/LoginPage.jsx
+++ src/Pages/LoginPage.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import firebase from 'firebase';
 import FirebaseAuth from 'react-firebaseui/FirebaseAuth';
+import { Typography, Box } from '@material-ui/core';
 
 const uiConfig = {
     // Popup signin flow rather than redirect flow.
@@ -20,7 +21,12 @@ const uiConfig = {
 
 function LoginPage(props) {
     return (
-        <FirebaseAuth uiConfig={uiConfig} firebaseAuth={firebase.auth()} />
+        <Box display="flex" alignItems="center" flexDirection="column">
+            <FirebaseAuth uiConfig={uiConfig} firebaseAuth={firebase.auth()} />
+            <Typography variant="body2">
+                If you're not an existing user, an account will be made once you enter an email or login with your Google account!
+            </Typography>
+        </Box>
     );
 }
 
