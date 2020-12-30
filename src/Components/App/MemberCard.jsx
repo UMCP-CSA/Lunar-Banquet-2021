883e91d4d5fa28425efda0c1153f571fd9958cf1
Alex Heng <alexheng@Huawei-MateBook-X-Pro.local>
2020-12-29 20:31:07 -0500

    Committee members fixed

--- src/Components/App/MemberCard.jsx
+++ src/Components/App/MemberCard.jsx
@@ -2,10 +2,11 @@ import React from "react";
 import { makeStyles } from "@material-ui/core";
 import { Typography, Grid, Container } from "@material-ui/core";
 
-const useStyles = makeStyles((theme) => ({
-    // container: {
-    //     backgroundColor: "#ffffff"
-    // }  
+const useStyles = makeStyles((theme) => ({ 
+    image: {
+        width: theme.spacing(40),
+        height: theme.spacing(40)
+    }
 }));
 
 
@@ -14,29 +15,29 @@ function MemberCard(props) {
     const classes = useStyles();
 
     return (
-        <Container className={classes.container}>
+        <Container >
             <Grid container spacing={3}>
                 
                 <Grid item xs={3}>
-                    <img src={props.image1} className="img-fluid" width="250" height="250" />    
+                    <img src={props.image1} className = {classes.image} />    
                     <Typography varient="h5">{props.name1}</Typography>
                     <Typography varient="h5">{props.bio1}</Typography>
                 </Grid>
 
                 <Grid item xs={3}>
-                    <img src={props.image2} className={classes.logo} width="250" height="250" />
+                    <img src={props.image2} className = {classes.image} />
                     <Typography varient="h5">{props.name2}</Typography>
                     <Typography varient="h5">{props.bio2}</Typography>
                 </Grid>
 
                 <Grid item xs={3}>
-                    <img src={props.image3} className={classes.logo} width="250" height="250" />
+                    <img src={props.image3} className = {classes.image} />
                     <Typography varient="h5">{props.name3}</Typography>
                     <Typography varient="h5">{props.bio3}</Typography>
                 </Grid>
 
                 <Grid item xs={3}>
-                    <img src={props.image4} className={classes.logo} width="250" height="250" />
+                    <img src={props.image4} className = {classes.image} />
                     <Typography varient="h5">{props.name4}</Typography>
                     <Typography varient="h5">{props.bio4}</Typography>
                 </Grid>
