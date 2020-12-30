5c4de179fce14a4d7cb9fe79138bb13d4c1230dc
Mei <meiluwork@gmail.com>
2020-12-29 19:00:45 -0500

    Update favicon and title

--- src/Pages/StreamPage.jsx
+++ src/Pages/StreamPage.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { makeStyles } from '@material-ui/core/styles'
-import { Grid, Container, Typography } from '@material-ui/core';
+import { Container } from '@material-ui/core';
 
 const useStyles = makeStyles((theme) => ({
     video: {
