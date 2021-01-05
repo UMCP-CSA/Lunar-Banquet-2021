a25f0f0d47e92b5df6f6b4fab64b2499440ce863
Nick Zhou <zhou.nicholas@outlook.com>
2021-01-05 12:12:42 -0500

    Style changes

--- src/Components/Shop/ShopTile.jsx
+++ src/Components/Shop/ShopTile.jsx
@@ -22,37 +22,38 @@ function ShopTile(props) {
     const classes = useStyles();
 
     return (
+        /* Stock badge */
         <Badge badgeContent={props.stock} color="primary" anchorOrigin={{ vertical: 'top', horizontal: 'right' }}>
             {/* // container for tile */}
             <Box className={classes.box} bgcolor="secondary.main" width={300} height={400}>
-                    <Grid container direction="column" alignItems="center">
-                        { /* picture of item */ }
-                        <Grid item>
-                            <img src={props.image} className={classes.image} alt={props.alt} width="200" height="200"/>
-                        </Grid>
-
-                        { /* item name */ }
-                        <Grid item>
-                            <Typography variant="h4">{props.name}</Typography>
-                        </Grid>
-
-                        { /* item price */ }
-                        <Grid item>
-                            <Typography variant="h5">{props.price}</Typography>
-                        </Grid>
-
-                        { /* item description */ }
-                        <Grid item>
-                            <Typography variant="body2">{props.desc}</Typography>
-                        </Grid>
-
-                        { /* add to cart button */ }
-                        <Grid item>
-                            <Button className={classes.button} variant="contained" color="primary" href={props.link}>
-                                Add To Cart
-                            </Button>
-                        </Grid>
+                <Grid container direction="column" alignItems="center">
+                    { /* picture of item */ }
+                    <Grid item>
+                        <img src={props.image} className={classes.image} alt={props.alt} width="200" height="200"/>
                     </Grid>
+
+                    { /* item name */ }
+                    <Grid item>
+                        <Typography variant="h4">{props.name}</Typography>
+                    </Grid>
+
+                    { /* item price */ }
+                    <Grid item>
+                        <Typography variant="h5">{props.price}</Typography>
+                    </Grid>
+
+                    { /* item description */ }
+                    <Grid item>
+                        <Typography variant="body2">{props.desc}</Typography>
+                    </Grid>
+
+                    { /* add to cart button */ }
+                    <Grid item>
+                        <Button className={classes.button} variant="contained" color="primary" href={props.link}>
+                            Add To Cart
+                        </Button>
+                    </Grid>
+                </Grid>
             </Box>
         </Badge>
     );
