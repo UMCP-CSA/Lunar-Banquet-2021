c07673ef633fe22dc701f53b756fde86508c5778
Nick Zhou <zhou.nicholas@outlook.com>
2021-01-04 23:16:56 -0500

    Tile bg-color, round edges, and stock badge

--- src/Components/Shop/ShopTile.jsx
+++ src/Components/Shop/ShopTile.jsx
@@ -1,8 +1,11 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import { Typography, Grid, Container, Avatar, Button } from "@material-ui/core";
+import { Box, Typography, Grid, Button, Badge } from "@material-ui/core";
 
 const useStyles = makeStyles((theme) => ({
+    box: {
+        borderRadius: '16px',
+    },
     productPic: {
         width: theme.spacing(30),
         height: theme.spacing(30)
@@ -10,6 +13,11 @@ const useStyles = makeStyles((theme) => ({
     image: {
         borderRadius: '50%',
         objectFit: 'cover'
+    },
+    button: {
+        marginTop: '10%',
+        marginBotton: '15%',
+        borderRadius: '5em'
     }
 }));
 
@@ -17,39 +25,39 @@ function ShopTile(props) {
     const classes = useStyles();
 
     return (
-        // container for tile
-        <Container>
-            {/* <Button variant="contained" color="secondary" href={props.link}> */}
-                <Grid container direction="column" alignItems="center">
-                    { /* picture of item */ }
-                    <Grid item>
-                        <img src={props.image} className={classes.image} alt={props.alt} width="300" height="300"/>
-                    </Grid>
+        <Badge badgeContent={props.stock} color="primary" anchorOrigin={{ vertical: 'top', horizontal: 'right' }}>
+            {/* // container for tile */}
+            <Box className={classes.box} bgcolor="secondary.main">
+                    <Grid container direction="column" alignItems="center">
+                        { /* picture of item */ }
+                        <Grid item>
+                            <img src={props.image} className={classes.image} alt={props.alt} width="300" height="300"/>
+                        </Grid>
 
-                    { /* item name */ }
-                    <Grid item>
-                        <Typography variant="h4">{props.name}</Typography>
-                    </Grid>
+                        { /* item name */ }
+                        <Grid item>
+                            <Typography variant="h4">{props.name}</Typography>
+                        </Grid>
 
-                    { /* item price */ }
-                    <Grid item>
-                        <Typography variant="p">{props.price}</Typography>
-                    </Grid>
+                        { /* item price */ }
+                        <Grid item>
+                            <Typography variant="h5">{props.price}</Typography>
+                        </Grid>
 
-                    { /* item description */ }
-                    <Grid item>
-                        <Typography variant="p">{props.desc}</Typography>
-                    </Grid>
+                        { /* item description */ }
+                        <Grid item>
+                            <Typography variant="body2">{props.desc}</Typography>
+                        </Grid>
 
-                    { /* add to cart button */ }
-                    <Grid item>
-                        <Button variant="contained" color="primary" href={props.link}>
-                            Add To Cart
-                        </Button>
+                        { /* add to cart button */ }
+                        <Grid item>
+                            <Button className={classes.button} variant="contained" color="primary" href={props.link}>
+                                Add To Cart
+                            </Button>
+                        </Grid>
                     </Grid>
-                </Grid>
-            {/* </Button> */}
-        </Container>
+            </Box>
+        </Badge>
     );
 }
 
--- src/Pages/ShopPage.jsx
+++ src/Pages/ShopPage.jsx
@@ -5,11 +5,10 @@ import TempBabyMilo from '../Assets/Images/ShopImages/TempBabyMilo.jpg';
 import TempBoba from '../Assets/Images/ShopImages/TempBoba.png';
 import TempSupreme from '../Assets/Images/ShopImages/TempSupreme.jpg';
 import TempOVO from '../Assets/Images/ShopImages/TempOVO.jpg';
+import { Grid } from "@material-ui/core";
 
 const useStyles = makeStyles((theme) => ({
     itemRow: {
-        display: 'flex',
-        flexDirection: 'row',
         marginTop: '5%'
     }
 }));
@@ -19,36 +18,48 @@ function ShopPage(props) {
     
     return (
         <div>
-            <li className={classes.itemRow}>
-                <ShopTile
-                    name="Boba"
-                    price="$5.00"
-                    desc="Yummy"
-                    image={TempBoba}
-                    alt="Boba cup"
-                />
-                <ShopTile
-                    name="Sticker"
-                    price="$10000.00"
-                    desc="Baby Milo"
-                    image={TempBabyMilo}
-                    alt="Astro Baby Milo"
-                />
-                <ShopTile
-                    name="Supreme Bag"
-                    price="$400.00"
-                    desc="Easy cop"
-                    image={TempSupreme}
-                    alt="Supreme bag"
-                />
-                <ShopTile
-                    name="OVO Cards"
-                    price="$22.00"
-                    desc="Must have"
-                    image={TempOVO}
-                    alt="OVO Cards"
-                />
-            </li>
+            <Grid container className={classes.itemRow} direction="row" justify="space-evenly" alignItems="center">
+                <Grid item>
+                    <ShopTile
+                        name="Boba"
+                        price="$5.00"
+                        desc="Yummy"
+                        image={TempBoba}
+                        alt="Boba cup"
+                        stock="In Stock"
+                    />
+                </Grid>
+                <Grid item>
+                    <ShopTile
+                        name="Sticker"
+                        price="$10000.00"
+                        desc="Baby Milo"
+                        image={TempBabyMilo}
+                        alt="Astro Baby Milo"
+                        stock="In Stock"
+                    />
+                </Grid>
+                <Grid item>
+                    <ShopTile
+                        name="Supreme Bag"
+                        price="$400.00"
+                        desc="Easy cop"
+                        image={TempSupreme}
+                        alt="Supreme bag"
+                        stock="In Stock"
+                    />
+                </Grid>
+                <Grid item>
+                    <ShopTile
+                        name="OVO Cards"
+                        price="$22.00"
+                        desc="Must have"
+                        image={TempOVO}
+                        alt="OVO Cards"
+                        stock="In Stock"
+                    />
+                </Grid>
+            </Grid>
         </div>
     )
 }
