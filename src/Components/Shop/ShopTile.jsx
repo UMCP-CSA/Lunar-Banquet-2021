459022926b2f39af05913afa860e81e8ac734791
Nick Zhou <zhou.nicholas@outlook.com>
2021-01-05 13:04:33 -0500

    Change stock to box

--- src/Components/Shop/ShopTile.jsx
+++ src/Components/Shop/ShopTile.jsx
@@ -3,13 +3,19 @@ import { makeStyles } from "@material-ui/core/styles";
 import { Box, Typography, Grid, Button, Badge } from "@material-ui/core";
 
 const useStyles = makeStyles((theme) => ({
-    box: {
+    outerBox: {
         borderRadius: '16px',
+        backgroundColor: theme.palette.primary.main
+    },
+    innerBox: {
+        borderRadius: '0px 8px 0px 8px',
+        backgroundColor: theme.palette.secondary.main,
+        paddingTop: '1%'
     },
     image: {
         borderRadius: '50%',
         objectFit: 'cover',
-        marginTop:'10%',
+        // marginTop:'-5%',
         marginBottom:'10%'
     },
     button: {
@@ -22,40 +28,42 @@ function ShopTile(props) {
     const classes = useStyles();
 
     return (
-        /* Stock badge */
-        <Badge badgeContent={props.stock} color="primary" anchorOrigin={{ vertical: 'top', horizontal: 'right' }}>
-            {/* // container for tile */}
-            <Box className={classes.box} bgcolor="secondary.main" width={300} height={400}>
-                <Grid container direction="column" alignItems="center">
-                    { /* picture of item */ }
-                    <Grid item>
-                        <img src={props.image} className={classes.image} alt={props.alt} width="200" height="200"/>
-                    </Grid>
-
-                    { /* item name */ }
-                    <Grid item>
-                        <Typography variant="h4">{props.name}</Typography>
-                    </Grid>
-
-                    { /* item price */ }
-                    <Grid item>
-                        <Typography variant="h5">{props.price}</Typography>
-                    </Grid>
-
-                    { /* item description */ }
-                    <Grid item>
-                        <Typography variant="body2">{props.desc}</Typography>
-                    </Grid>
-
-                    { /* add to cart button */ }
-                    <Grid item>
-                        <Button className={classes.button} variant="contained" color="primary" href={props.link}>
-                            Add To Cart
-                        </Button>
-                    </Grid>
+        /* // container for tile */
+        <Box className={classes.outerBox} width={300} height={400}>
+            <Grid container direction="row" justify="flex-end">
+                <Box className={classes.innerBox} alignItems="right" width={100} height={20}>
+                    <Typography variant="body2">{props.stock}</Typography>
+                </Box>
+            </Grid>
+            <Grid container direction="column" alignItems="center">
+                { /* picture of item */ }
+                <Grid item>
+                    <img src={props.image} className={classes.image} alt={props.alt} width="200" height="200"/>
                 </Grid>
-            </Box>
-        </Badge>
+
+                { /* item name */ }
+                <Grid item>
+                    <Typography variant="h4">{props.name}</Typography>
+                </Grid>
+
+                { /* item price */ }
+                <Grid item>
+                    <Typography variant="h5">{props.price}</Typography>
+                </Grid>
+
+                { /* item description */ }
+                <Grid item>
+                    <Typography variant="body2">{props.desc}</Typography>
+                </Grid>
+
+                { /* add to cart button */ }
+                <Grid item>
+                    <Button className={classes.button} variant="contained" color="primary" href={props.link}>
+                        Add To Cart
+                    </Button>
+                </Grid>
+            </Grid>
+        </Box>
     );
 }
 
