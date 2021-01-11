ac9311ea5610d18d66681e6efca08eb3f96d3e5c
Stanley Zhu <stanleyzhu53@gmail.com>
2021-01-11 14:50:00 -0500

    added enter button to homepage

--- src/Components/Enter/EnterButton.jsx
+++ src/Components/Enter/EnterButton.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
-import { Grid, Link } from "@material-ui/core";
+import { Link } from "@material-ui/core";
 import { makeStyles } from '@material-ui/core/styles';
 
 const buttonStyles = makeStyles({
   buttonImage: {
+    // rounded box
     borderRadius: '25px',
+    // add shadow
     boxShadow: '6px 8px #545454',
+    // bring component to front
+    position: "relative",
+    zIndex: 2,
 }
 });
 
@@ -13,24 +18,15 @@ function EnterButton(props) {
   const classes = buttonStyles();
   
   return(
-      <Grid 
-        container
-        spacing={0}
-        alignItems="center"
-        justify="center"
-      >
-        <Grid item>
-          <Link href={props.link}>
-            <img 
-              src={props.img}
-              className={classes.buttonImage} 
-              alt={props.alt} 
-              width={props.width}
-              height={props.height}/>
-          </Link>
-        </Grid>
-      </Grid>
-      
+    <Link href={props.link}>
+      <img
+        src={props.img}
+        className={classes.buttonImage} 
+        alt={props.alt}
+        width={props.width}
+        height={props.height}
+      />
+    </Link>
   )
 }
 
